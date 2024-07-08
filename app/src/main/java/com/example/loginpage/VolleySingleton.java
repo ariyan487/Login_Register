5837e5295680877ab189986432744c09feffcbf3
A　R　I　Y　A　 N　ツ <139285103+ariyan487@users.noreply.github.com>
2024-07-08 15:04:40 +0600

    Update VolleySingleton.java

--- app/src/main/java/com/example/loginpage/VolleySingleton.java
+++ app/src/main/java/com/example/loginpage/VolleySingleton.java
@@ -1,7 +1,6 @@
 package com.example.loginpage;
 
 import android.content.Context;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
